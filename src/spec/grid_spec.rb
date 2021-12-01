747d3c560e093549ff486aa068035e9a33f2f81c
Danyherz42 <93446468+dany-herz42@users.noreply.github.com>
2021-11-30 18:43:52 -0600

    Update grid_spec.rb
    - Generate_matrix test added: Evaluate matrix not equal nil
    - Draw test: First version of test to draw method
    
    Co-Authored-By: Angel Isaac <50933273+Anisbeflo@users.noreply.github.com>
    Co-Authored-By: Josue Samano <83667083+Joshsam31@users.noreply.github.com>

--- src/spec/grid_spec.rb
+++ src/spec/grid_spec.rb
@@ -11,11 +11,25 @@ RSpec.describe initialize do
   end
 end
 
-RSpec.describe generate_matrix do
+RSpec.describe Grid do
   context 'The generation of the matrix' do
     it 'Matrix is not equal to nil' do
       grid_test2 = Grid.new(3, 5)
-      expect(grid_test2[0][0]).to eq !nil
+      grid_test2.generate_matrix
+      expect(grid_test2.grid[0][0].nil?).to eq false    
     end
   end
 end
+
+RSpec.describe Grid do
+  context 'draw the matrix' do
+    it '' do
+      grid_test3 = Grid.new(3,5)
+      grid_test2.generate_matrix
+      grid_test3.draw()
+      expect(grid_test3.grid[3][5] == grid_test3.grid_char[3][5] ? ).to eq true 
+    end
+  end 
+end
+ 
+
